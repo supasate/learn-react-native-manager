72be1f054c83808e5d1dceb0c2bcf31aa02c94f9
Supasate Choochaisri <supasate.c@gmail.com>
2017-01-05 00:44:45 +0700

    Create commit

---
+++ config/index.template.js
@@ -0,0 +1,11 @@
+const config = {
+  firebase: {
+    apiKey: '<api_key>',
+    authDomain: '<domain>.firebaseapp.com',
+    databaseURL: 'https://<database_url>.firebaseio.com',
+    storageBucket: '<storage_bucket>.appspot.com',
+    messagingSenderId: '<sender_id>',
+  },
+}
+
+export default config

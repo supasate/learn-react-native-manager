47048206aff8d525b411f5a68e2434154fcd13f4
Supasate Choochaisri <supasate.c@gmail.com>
2017-01-05 16:31:16 +0700

    Clearn employee form after created

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,4 +1,4 @@
-import { EMPLOYEE_UPDATE } from '../actions/types'
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from '../actions/types'
 
 const INITIAL_STATE = {
   name: '',
@@ -16,6 +16,8 @@ export default (state = INITIAL_STATE, action) => {
         [prop]: value,
       }
     }
+    case EMPLOYEE_CREATE:
+      return INITIAL_STATE
     default:
       return state
   }

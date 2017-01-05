c6b8934ebd750107b7d76d56677df3944cb0d91f
Supasate Choochaisri <supasate.c@gmail.com>
2017-01-05 23:04:51 +0700

    Text message

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { text } from 'react-native-communications'
 import EmployeeForm from './EmployeeForm'
 import { Card, CardSection, Button } from './common'
 import { employeeUpdate, employeeSave } from '../actions'
@@ -18,13 +19,26 @@ class EmployeeEdit extends Component {
     this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid })
   }
 
+  onTextPress() {
+    const { phone, shift } = this.props
+
+    text(phone, `Your upcoming is on ${shift}`)
+  }
+
   render() {
     return (
       <Card>
         <EmployeeForm />
+
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Save Changes
+          </Button>
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={this.onTextPress.bind(this)}>
+            Text Schedule
           </Button>
         </CardSection>
       </Card>
